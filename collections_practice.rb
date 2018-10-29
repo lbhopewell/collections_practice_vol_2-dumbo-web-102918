549b9fcf24f18d4b3951a30f63eba41713861885
Bruce Hopewell <lbhopewell@outlook.com>
2018-10-29 02:30:40 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -22,7 +22,8 @@ results
 end
 
 def first_wa(array)
-  result = array.find {|item| item.start_with?("wa")}
+  string_array = 
+  result = string_array.find {|item| item.start_with?("wa")}
   return result
 end
 
