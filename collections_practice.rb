23f3d7ad992c978be6e7f5ad5108a48f7b74362b
Bruce Hopewell <lbhopewell@outlook.com>
2018-10-29 02:11:35 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,35 @@
 # your code goes here
+require "pry"
+
+def begins_with_r(tools)
+  result = 0
+  tools.collect do |x| 
+    if x.start_with?("r") == false 
+    result += 1
+end
+end
+if result == 0 
+  return true
+else
+  return false
+end
+end
+
+def contain_a(array)
+  #result = []
+  results = array.select {|x| x.include?("a")}
+results
+end
+
+def first_wa(array)
+  list = array.select do |x| 
+  if x.start_with?("wa")
+  #binding.pry
+  return list
+end
+end
+end
+
+
+
+
