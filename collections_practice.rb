78e50277aa65e967d13fd5afa7771e93dd418ea0
Bruce Hopewell <lbhopewell@outlook.com>
2018-10-29 04:42:55 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -44,7 +44,7 @@ end
 
 def count_elements(array)
  count_result = []
- array.each do |name, data|
+ array.each do |data|
     hit = array.count(data)
    count_result << {:count=>(hit), :name=>(data)}
   end
